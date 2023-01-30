6f51a3247aeb6936db32264c84938d1bd078bb7a
Bogdan <bogdangaponenko1996@gmail.com>
2023-01-30 11:40:26 -0600

    solve 1 task and add simplelightbox npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import simpleLightBox from 'simplelightbox';
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const createGalleryItem = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+  />
+</a>
+`;
+  })
+  .join('');
+
+gallery.innerHTML = createGalleryItem;
+
+new simpleLightBox('.gallery a', {
+  captionsData: 'alt',
+})();

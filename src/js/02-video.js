d351ff0c8070d628d96d401bee87e3747920c35d
Bogdan <bogdangaponenko1996@gmail.com>
2023-02-02 14:06:34 -0600

    fix 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,15 +8,15 @@ const player = new Player(iframe);
 
 player.on('timeupdate', throttle(saveLocalStorage, 600));
 
-player.setCurrentTime(loadLocalStorageValue(keyForLocalStorage));
+updateTime(keyForLocalStorage);
 
 function saveLocalStorage({ seconds }) {
   localStorage.setItem(keyForLocalStorage, seconds);
 }
 
-function loadLocalStorageValue(key) {
-  const getLocalStorageKey = localStorage.getItem(key);
-  return getLocalStorageKey === null
-    ? undefined
-    : JSON.parse(getLocalStorageKey);
+function updateTime(key) {
+  const getCurrentTime = localStorage.getItem(key);
+  if (getCurrentTime) {
+    player.setCurrentTime(getCurrentTime);
+  }
 }
